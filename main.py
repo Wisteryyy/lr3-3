8211229355d251114a0d94074d7e303e589e395d
Sofia Skakun <sofiaskakun30@gmail.com>
2024-10-20 17:45:08 +0300

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-d=int(input("Введите день: "))
-m=int(input("Введите месяц: "))
+d=int(input("Введите день: ")) # вводим день
+m=int(input("Введите месяц: ")) # вводим месяц
 
-def season(d, m):
+def season(d, m): # при помощи def создаем функцию которая при помощи if, elif, else определяет к какому времени года относится дата
     if (m == 3 and d >= 1) or (m == 4) or (m == 5) or (m == 6 and d < 1):
         return "Весна"
     elif (m == 6 and d >= 1) or (m == 7) or (m == 8) or (m == 9 and d < 1):
@@ -11,5 +11,5 @@ def season(d, m):
     else:
         return "Зима"
 
-vremya_goda=season(d,m)
-print("Эта дата относится к сезону", vremya_goda)
+vremya_goda=season(d,m) # присваиваем переменной значение функции season
+print("Эта дата относится к сезону", vremya_goda) # выводи переменную
